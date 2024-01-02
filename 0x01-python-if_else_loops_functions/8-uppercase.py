6c96f7a61ce53400916ee23c9da4bd6474c8cbf9
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-01-02 08:46:59 +0200

    add 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def uppercase(str: str):
+    res = ""
+    for c in str:
+        if "a" <= c <= "z":
+            res += chr(ord(c) - 32)
+        else:
+            res += c
+
+    print(res)
