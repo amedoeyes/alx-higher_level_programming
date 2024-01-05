5385985a08a1c1f00bf78c3a54568faf5459324c
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-01-05 14:12:50 +0200

    add 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    print(
+        "\n".join(
+            " ".join("{:d}".format(col) for col in row) for row in matrix
+        )
+    )
