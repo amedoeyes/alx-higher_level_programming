32aa4f362380b9979a224c96fb1b0083d74063b8
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-01-25 02:46:07 +0200

    add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""A module that defines a class Square"""
+
+
+class Square:
+    """Square class"""
+
+    __size = 0
+
+    def __init__(self, size):
+        self.__size = size
