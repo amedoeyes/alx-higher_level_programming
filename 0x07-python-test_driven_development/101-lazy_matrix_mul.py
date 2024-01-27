ef0dc8c9ebc8ae01ad5e559154866b9f55729f1e
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-01-27 14:13:30 +0200

    add 101-lazy_matrix_mul.py

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines lazy_matrix_mul function"""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiply two matrices"""
+
+    return np.matmul(m_a, m_b)
