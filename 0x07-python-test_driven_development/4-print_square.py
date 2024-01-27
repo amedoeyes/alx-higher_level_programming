6c28c3ad31ae34f7066799f4e015dfd8a557ba73
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-01-27 12:27:41 +0200

    add 4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defines print_square function"""
+
+
+def print_square(size):
+    """Prints a square with the character #"""
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
