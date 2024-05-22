521e3f06764805bce1c7a2493df090eb31b002db
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-05-23 01:23:36 +0300

    add 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+request(url, function (error, _, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(file, body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
